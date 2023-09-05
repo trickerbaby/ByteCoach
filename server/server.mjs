ab45904251794ea391ad5d645e055f4ed5ef45f0
trickerbaby <nnnanu2@gmail.com>
2023-09-06 00:11:29 +0530

    FEAT --API update

--- server/server.mjs
+++ server/server.mjs
@@ -1,18 +1,93 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser  from 'body-parser';
 import OpenAI from 'openai'; // Import OpenAI modules
+
+
+import {MongoClient,ServerApiVersion} from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const password = "Navya#1427";
+const enpassword = encodeURIComponent("Navya#1427");
+const uri = `mongodb+srv://trickerbaby:${enpassword}@cluster0.rq5ucba.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
-const openai = new OpenAI({apiKey: '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'});
+
+const openai = new OpenAI({apiKey: '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'});
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome!' });
 });
+
+
+app.get('/getquestion',async (req,res)=>{
+  const feed = req.query.feed;
+  const username = req.query.username;
+  const day = req.query.day;
+ 
+
+  try{
+    await client.connect();
+    const db = client.db('ByteCoach');
+    const col = db.collection('Users');
+    var roadmap = await col.findOne({username});
+  
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
+ 
+  console.log("todays topic is "+roadmap.roadmap[parseInt(day)]);
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `Give 1 'C programming' question on the base of this topic ${roadmap.roadmap[parseInt(day)]} make a question that makes me improve on my mistakes and these are some of my past feedbacks ${feed}`,
+      },
+    ],
+  });
+
+  console.log("AI request processed");
+
+  var question = completion.choices[0].message.content;
+
+  console.log(question);
+  res.json({question});
+
+
+
+})
+
 
 app.get('/personalroadmap', async (req, res) => {
   const sub = req.query.subject;
@@ -20,6 +95,8 @@ app.get('/personalroadmap', async (req, res) => {
   const exp = req.query.exp;
   const aim = req.query.aim;
   const hours = req.query.hours;
+  const username = req.query.username;
+
 
   const completion = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
@@ -31,9 +108,58 @@ app.get('/personalroadmap', async (req, res) => {
     ],
   });
 
+  console.log("AI request processed");
+
+  var roadmap = completion.choices[0].message.content.split('#');
+
+  console.log(roadmap);
   
-  console.log(completion.choices[0].message);
-  res.json({ roadmap: completion.choices[0].message });
+  
+
+  try{
+    await client.connect();
+    const db = client.db('ByteCoach');
+    const usersCollection = db.collection('Users');
+    await usersCollection.insertOne({username , roadmap});
+  }
+  catch(err)
+  {
+    console.log("Error uploading to DB",err);
+  }
+
+  res.json({ roadmap: roadmap });
+});
+
+app.get('/teachme',async (req,res) => {
+  const username = req.query.username;
+  const day = req.query.day;
+  
+  try
+  {
+    await client.connect();
+    const db = client.db('ByteCoach');
+    const col = db.collection('Users');
+
+    var roadmap = await col.findOne({username});
+    console.log(roadmap.roadmap[parseInt(day)]);
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `Teach me this topic in a friendly manner -> ${roadmap.roadmap[parseInt(day)]}`,
+      },
+    ],
+  });
+  console.log(completion.choices[0].message.content);
+  res.json({theory:completion.choices[0].message.content });
+
 });
 
 app.get('/practice', (req, res) => {
